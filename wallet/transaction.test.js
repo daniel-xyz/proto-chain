67083421df63620c472ab8cb2d8e1c05104023d8
Daniel Bischoff <danielbischoff@hotmail.com>
2018-06-14 11:59:11 +0200

    verify transactions signatures

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -23,6 +23,16 @@ describe('Transaction', () => {
     expect(transaction.input.amount).toEqual(wallet.balance)
   })
 
+  it('valides a valid transaction', () => {
+    expect(Transaction.verifyTransaction(transaction)).toBe(true)
+  })
+
+  it('invalidates a corrupt transaction', () => {
+    transaction.outputs[0].amount = 700000
+
+    expect(Transaction.verifyTransaction(transaction)).toBe(false)
+  })
+
   describe('transacting with an amount that exceeds the balance', () => {
     beforeEach(() => {
       amount = 50000
