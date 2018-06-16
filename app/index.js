7a15142145a99c930ee1e41c737771fe97660a3a
Daniel Bischoff <danielbischoff@hotmail.com>
2018-06-16 16:59:58 +0200

    add endpoints for transactions

--- app/index.js
+++ app/index.js
@@ -2,17 +2,32 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const Blockchain = require('../blockchain')
 const P2pServer = require('./p2p-server')
+const Wallet = require('../wallet')
+const TransactionPool = require('../wallet/transaction-pool')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001
 
 const app = express()
 const bc = new Blockchain()
+const wallet = new Wallet()
+const tp = new TransactionPool()
 const p2pServer = new P2pServer(bc)
 
 app.use(bodyParser.json())
 
 app.get('/blocks', (req, res) => {
   res.json(bc.chain)
+})
+
+app.get('/transactions', (req, res) => {
+  res.json(tp.transactions)
+})
+
+app.post('/transact', (req, res) => {
+  const { recipient, amount } = req.body
+  const transaction = wallet.createTransaction(recipient, amount, tp)
+
+  res.redirect('/transactions')
 })
 
 app.post('/mine', (req, res) => {
