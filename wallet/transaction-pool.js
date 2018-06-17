ca7f54a85eeb1000bc5318869d56d566c379f064
Daniel Bischoff <danielbischoff@hotmail.com>
2018-06-17 14:39:11 +0200

    mine transactions from pool

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,3 +1,5 @@
+const Transaction = require('./transaction')
+
 class TransactionPool {
   constructor() {
     this.transactions = []
@@ -15,6 +17,19 @@ class TransactionPool {
 
   existingTransaction(address) {
     return this.transactions.find(t => t.input.address === address)
+  }
+
+  validTransactions() {
+    return this.transactions.filter(t => {
+      const outputTotal = t.outputs.reduce((total, output) => {
+        total + output.amount
+      }, 0)
+
+      if (transaction.input.amount !== outputTotal) return console.log(`Invalid transaction from ${transaction.input.address}`)
+      if (!Transaction.verifyTransaction(transaction)) return console.log(`Invalid signature from ${transaction.input.address}`)
+
+      return transaction
+    })
   }
 }
 
